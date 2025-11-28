ee7d82d7d4edbe088322a254959b5513b0505845
Maryem <meryembannour3@gmail.com>
2025-11-28 11:56:51 +0100

    Add basePath for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
   images: {
     unoptimized: true,
   },
   trailingSlash: true,
   reactStrictMode: true,
+  basePath: "/Myportfolio2", 
+  assetPrefix: "/Myportfolio2/",
 };
 
 export default nextConfig;
